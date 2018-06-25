e547f3b6a7ea076c11e71111e5c913f381dedf91
unknown <luisd824@gmail.com>
2018-06-25 17:00:26 -0400

    NOTES APP
    create note model
    
    migrated note app and tested

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from uuid import uuid4
+
+
+class Note(models.Model):
+    # TODO: Add author
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    # TODO: Maybe add categories?
