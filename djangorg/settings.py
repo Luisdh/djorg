76557793e6b1f454857ebd443f61660318337b34
unknown <luisd824@gmail.com>
2018-06-29 18:07:25 -0400

    change to setting DATABASE_URL

--- djangorg/settings.py
+++ djangorg/settings.py
@@ -84,11 +84,10 @@ WSGI_APPLICATION = 'djangorg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        dj_database_url.config(default=config('DATABASE_URL'))
     }
 }
-DATABASES['default'] = dj_database_url.config(default='DATABASE_URL')
+# DATABASES['default'] = dj_database_url.config(default='DATABASE_URL')
 
 
 # Password validation
