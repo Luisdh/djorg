fda38ab629e55ff66d0821f549f777d6dc39afd3
unknown <luisd824@gmail.com>
2018-06-29 13:23:07 -0400

    change to databases

--- djangorg/settings.py
+++ djangorg/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'] = dj_database_url.config(DATABASE_URL, conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(default='DATABASE_URL')
 
 
 # Password validation
